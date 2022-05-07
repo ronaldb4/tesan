4a51850063e197e524985c5a00e20696a7bf2caa
ronaldb4 <66567120+ronaldb4@users.noreply.github.com>
2022-05-06 22:06:25 -0500

    - update README.md to include skip and random-interval

--- run_remain.sh
+++
@@ -1,8 +0,0 @@
-cd src/concept_embedding
-#python ../train_embedding.py --model skip_gram --configSet concept_embedding/mimic_paper.json
-python ../train_embedding.py --model skip_gram --configSet concept_embedding/cms_paper.json
-#python ../train_embedding.py --model random_interval --configSet concept_embedding/mimic_paper.json
-python ../train_embedding.py --model random_interval --configSet concept_embedding/cms_paper.json
-cd ../../src/mortality_prediction
-#python ../train_prediction.py --model skip_gram --configSet mortality_prediction/mimic_paper.json
-#python ../train_prediction.py --model random_interval --configSet mortality_prediction/mimic_paper.json
